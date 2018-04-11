daa986a04543e74d2a64f226786d57af2bbb77cb
Grace <1525436188@qq.com>
2018-04-11 21:29:36 +0800

    created comment model and hooked up comments with our views.py and home_page.html template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import Comment
+
 
 def home_page(request):
+    comments = Comment.objects.all()
+
 
     if request.method == 'POST':
-        comment = request.POST.get('comment')
-        user = request.POST.get("username")
+        user_comment = request.POST.get('comment')
+        user_username = request.POST.get("username")
 
-        print(comment * 10)
-        return render(request, 'blog/home_page.html', {'comment': comment, 'username': user})
+        comment_obj = Comment.objects.create(
+            content=user_comment,
+            user=user_username
+        )
 
-    return render(request, 'blog/home_page.html')
+
+        print(comment_obj.content * 10)
+        return render(request, 'blog/home_page.html', {'comments': comments})
+
+
+    if request.method == "GET":
+        return render(request, 'blog/home_page.html', {'comments': comments} )
